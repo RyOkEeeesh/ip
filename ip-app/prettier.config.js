5723affd8a008a3db9b570b0fbb86738039d1344
RyOkEeeesh <dafe00842202@outlook.jp>
2025-11-12 14:01:14 +0900

    update

---
+++ ip-app/prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
---
+++ ip-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-indigo-500 to-pink-500 text-4xl font-bold text-white">
+        Tailwind v4 Working! 🚀
+      </div>
+    </>
+  );
+}
