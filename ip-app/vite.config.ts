3706d294ee4402d83a0f8025b87b37da172e994f
RyOkEeeesh <dafe00842202@outlook.jp>
2025-12-17 14:03:09 +0900

    update

--- ip-app/vite.config.ts
+++ ip-app/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ip/',
   plugins: [tailwindcss(), react()],
   server: {
     port: 3000
